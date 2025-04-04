7f5e698976ab15ba6863509aae8eacc7daf1feca
Hitansh Bhatt <74229658+HitanshBhatt@users.noreply.github.com>
2025-04-04 18:52:58 -0400

    Delete src directory

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from .baseline import baseline_model
-from .predict import predict
-from .utils import preprocess_data
-from .data_loader import load_data
-from .train import train_data
-
-__version__ = "0.1.0"
-__author__ = "Team27"
--- src/predict.py
+++
@@ -1,4 +0,0 @@
-from config import *
-
-def predict(model, X_test):
-    return model.predict(X_test)
